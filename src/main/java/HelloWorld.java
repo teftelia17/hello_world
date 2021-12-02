ddeb7bf19282ce51188a0429ff9aebc6fb7f489b
unknown <C:\Users\rvg\AppData\Roaming\The Bat!>
2021-12-02 17:52:51 +0200

    update Git

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,6 @@ package main.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World, Vlad");
+        MeetWithGit.main(args);
     }
 }
